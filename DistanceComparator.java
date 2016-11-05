fbc93b82fe5f222ea246985783600ef329a15881
xiaopeng9275 <350782466@qq.com>
2016-11-05 10:40:32 +0800

    Add files via upload

---
+++ DistanceComparator.java
@@ -0,0 +1,32 @@
+package HBaseIA.GIS.model;
+
+import java.awt.geom.Point2D;
+import java.util.Comparator;
+
+import org.apache.log4j.Logger;
+/**
+ * 自定义距离比较器
+ * @author 大象
+ *
+ */
+public class DistanceComparator implements Comparator<QueryMatch> {
+  static final Logger LOG = Logger.getLogger(DistanceComparator.class);
+
+  Point2D origin;
+
+  public DistanceComparator(double lon, double lat) {
+    this.origin = new Point2D.Double(lon, lat);
+  }
+
+  public int compare(QueryMatch o1, QueryMatch o2) {
+    if(Double.isNaN(o1.distance)) {
+      o1.distance = origin.distance(o1.lon, o1.lat);
+    }
+    if (Double.isNaN(o2.distance)) {
+      o2.distance = origin.distance(o2.lon, o2.lat);
+    }
+    if (o1.distance < 0 || o2.distance < 0)
+      LOG.warn("negative distance detected!");
+    return Double.compare(o1.distance, o2.distance);
+  }
+}
---
+++ QueryMatch.java
@@ -0,0 +1,25 @@
+package HBaseIA.GIS.model;
+/**
+ * 
+ * @author 大象
+ *
+ */
+public class QueryMatch {
+  public String id;
+  public String hash;
+  public double lon, lat;
+  public double distance = Double.NaN;
+
+  public QueryMatch(String id, String hash, double lon, double lat) {
+    this.id = id;
+    this.hash = hash;
+    this.lon = lon;
+    this.lat = lat;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("<QueryMatch: %4s, %12s, %3.4f, %3.4f, %2.5f >",
+                         id, hash, lon, lat, distance);
+  }
+}
